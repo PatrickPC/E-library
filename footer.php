ae440c0fa524d916114ab8536b2a80924fd7494e
Patrick P.C. <pratikchaudhary270@gmail.com>
2025-07-22 22:33:34 +0545

    Footer Contents

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+    <div class="container">
+            <div class="footer-content">
+               <h3>Contact Us</h3>
+               <p> <i class="fas fa-phone"></i> 084-400004/084-400003 </p>
+               <p> <i class="fa-solid fa-radio"></i> Radio Gurbaba 89.7 MHz </p>
+               <p> <i class="fas fa-envelope"></i> pratikchaudhary270@gmail.com </p>
+               <p> <i class="fas fa-map-marker-alt"></i> Bardiya, Nepal </p>
+            </div>
+            <div class="footer-content">
+                <h3>Quick Links</h3>
+                 <ul class="list">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#affilates">Affilates</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="books.php">Books</a></li>
+                    <li><a href="login.php">login</a></li>
+                 </ul>
+            </div>
+            <div class="footer-content">
+                <h3>Follow Us</h3>
+                <ul class="social-icons">
+                 <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                 <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                 <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                 <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                </ul>
+                </div>
+        </div>
+        <div class="bottom-bar">
+            <p>&copy; © 2080 Radio Gurbaba 89.7 MHz | All Rights Reserved.</p>
+    </div>
+</footer>
